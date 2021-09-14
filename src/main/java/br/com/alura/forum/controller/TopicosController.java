447197d2c682fc4d417ee77d35c9e6f18428cc55
LFernandoPP <87454966+LFernandoPP@users.noreply.github.com>
2021-09-14 19:26:05 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,36 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/topicos") //sem31
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @GetMapping
+  //  @RequestMapping(value = "/topicos", method = RequestMethod.GET)//sem31
+    public List<TopicoDTO> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDTO.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+            return TopicoDTO.converter(topicos);
+        }
+    }
+
+    @PostMapping
+  //  @RequestMapping(value = "/topicos", method = RequestMethod.POST)//sem31
+    public void cadastrar(TopicoForm topico) {
+
+    }
+}
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,7 @@
+package br.com.alura.forum.controller.form;
+
+public class TopicoForm {
+    private  String titulo;
+  //  private  String mensagem;
+  //  private
+}
